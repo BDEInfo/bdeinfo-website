cc2f4ba8e40da0152cf160c5f91b8ed1a8cb4898
Bartholomé <bartho.gili@gmail.com>
2022-01-12 21:00:22 +0100

    refractor(config): api base url

--- client/config/connection.js
+++ client/config/connection.js
@@ -1,6 +1,6 @@
 const apiURL = {
-    development: 'https://bdeinfo-api.barthofu.com',
-    production: 'https://bdeinfo-api.barthofu.com'
+    development: 'https://bdeinfo.api.barthofu.com',
+    production: 'https://bdeinfo.api.barthofu.com'
 }
 
 export default apiURL[process.env.NODE_ENV]
