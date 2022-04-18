646092afd14fc0fbf2f212e2f01fecd67a7e0a3a
Bartholomé <bartho.gili@gmail.com>
2022-04-18 18:45:28 +0200

    fix(client): bde members order and role output

--- client/src/components/modules/BDEMembers/BDEMembers.jsx
+++ client/src/components/modules/BDEMembers/BDEMembers.jsx
@@ -2,15 +2,17 @@ import styles from './BDEMembers.module.sass'
 
 import { getImage } from '@util/image'
 
+const sortBDEMembers = (bdeMembers) => bdeMembers.sort((a, b) => b.poste.weight - a.poste.weight)
+
 export default function BDEMember ({ bdeMembers }) {
 
     return (<>
         <ul className={styles.members}>
-            { bdeMembers.map(bdeMember => (
+            { sortBDEMembers(bdeMembers).map(bdeMember => (
                 <li className={styles.member}>
                     <img className={styles.image} src={getImage(bdeMember.image)} />
                     <div className={styles.name}>{bdeMember.firstName} {bdeMember.lastName}</div>
-                    <div className={styles.role}>{bdeMember.role}</div>
+                    <div className={styles.role}>{bdeMember.poste.grade} {bdeMember.poste.name}</div>
                 </li>
             )) }
         </ul>
